4e1e30e5cdf44eee2a1002995d655d551a9ee4d2
Hassan Akbar <hasxanblaze@gmail.com>
2022-05-28 12:57:53 +0500

    Created Login/Register Screen. Need to add a header at top yet.

--- web-project/src/App.js
+++ web-project/src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Fragment } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render(){
+    return (
+      <LoginRegister></LoginRegister>
+    )
+  }
 }
+
+class LoginRegister extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoginOpen: true
+    };
+  }
+
+  showLoginBox() {
+    this.setState({isLoginOpen: true});
+  }
+
+  showRegisterBox() {
+    this.setState({isLoginOpen: false});
+  }
+
+  render() {
+
+    return (
+      <Fragment>
+        <div class="box-container">
+          {this.state.isLoginOpen && <LoginBox/>}
+          {!this.state.isLoginOpen && <RegisterBox/>}
+        </div>
+        <div class="mx-auto" style={{width:'400px'}}>
+          {!this.state.isLoginOpen && <div
+            class="btn btn-primary"
+            onClick={this
+            .showLoginBox
+            .bind(this)}>
+            Login
+          </div>}
+          {this.state.isLoginOpen && <div
+            class="btn btn-primary"
+            onClick={this
+            .showRegisterBox
+            .bind(this)}>
+            Register
+          </div>}
+        </div>
+      </Fragment>
+      
+    );
+
+  }
+
+}
+
+
+//Login Box
+class LoginBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitLogin(e) {}
+
+  render() {
+    return (
+      <div class="mx-auto" style={{width:'400px', marginTop:'250px'}}>
+        <div class="header">
+          Login to Brilliant Pro
+        </div>
+        <div class="box">
+
+          <div class="mb-3">
+            <label class="form-label" htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              class="form-control"
+              placeholder="Username"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label" htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              class="form-control"
+              placeholder="Password"/>
+          </div>
+
+          <button
+            type="submit"
+            class="btn btn-primary"
+            onClick={this
+            .submitLogin
+            .bind(this)}>Login</button>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+//Register Box 
+class RegisterBox extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  submitRegister(e) {}
+
+  render() {
+    return (
+      <div class="mx-auto" style={{width:'400px', marginTop:'250px'}}>
+        <div class="header">
+          Register to Brilliant Pro
+        </div>
+        <div class="box">
+
+          <div class="mb-3">
+            <label class="form-label" htmlFor="username">Username</label>
+            <input
+              type="text"
+              name="username"
+              class="form-control"
+              placeholder="Username"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label" htmlFor="email">Email</label>
+            <input type="email" name="email" class="form-control" placeholder="Email"/>
+          </div>
+
+          <div class="mb-3">
+            <label class="form-label" htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              class="form-control"
+              placeholder="Password"/>
+          </div>
+          <button
+            type="submit"
+            class="btn btn-primary"
+            onClick={this
+            .submitRegister
+            .bind(this)}>Register</button>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 export default App;
