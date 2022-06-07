ef6920ebe2f677e36640742382d8b10bf4c3bf7c
Usman <usmanjanjuaa3@gmail.com>
2022-06-07 21:33:34 +0500

    removed errors

--- web-project/lms/src/server/app.js
+++ web-project/lms/src/server/app.js
@@ -1,10 +1,11 @@
 // Requiring module
+const express = require("express");
+
 var router = express.Router();
 const mongoose = require('mongoose');
 //const User = mongoose.model('user');
 
 
-const express = require("express");
 const path = require("path");
 var bodyParser = require("body-parser");
 
@@ -43,35 +44,6 @@ app.post("/api/courses", upload.single("courseImage"), function (req, res) {
   res.end("File not uploaded");
 });
 
-<<<<<<< HEAD
-//for register 
-app.post("/api/register" , function(req,res){
-
-
-  if(req.file){
-
-    var user = new User();
-    user.userName = req.body.userName ;
-    user.password = req.body.password ; 
-
-    user.email = req.body.email ;
-    user.phone = req.body.phone ;
-    
-    user.address = req.body.address ;
-    user.fullName = req.body.fullName ;
-    
-    user.dob = req.body.dob ;
-    user.registered = req.body.registered ;  
-   
-    user.save((err, doc) => {
-        if (!err)
-            res.redirect('/login');
-    });
-
-  }
-
-}); 
-=======
 app.post(
   "/api/user/register",
   upload.single("profilePic"),
@@ -88,7 +60,6 @@ app.post(
 );
 
 //http://localhost:44444/api/user/register
->>>>>>> 53dd4c2228c1a7dd1ba557feeb45231f716cfb3b
 
 // Server setup
 app.listen(PORT, () => {
